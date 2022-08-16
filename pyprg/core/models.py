7a70a20fb2c16a1cc4a02100702a9fb57d822adc
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2022-08-16 08:21:10 -0300

    Correct the last commit

--- pyprg/core/models.py
+++ pyprg/core/models.py
@@ -1,3 +1,20 @@
-# from django.db import models
+from django.db import models
+
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=128)
+    edition = models.PositiveIntegerField()
+    publication_year = models.CharField(max_length=4)
+    # um livro pode ter varios autores/autores podem ter varios livros
+    authors = models.ManyToManyField(Author, related_name='books')
+
+    def __str__(self):
+        return self.name
