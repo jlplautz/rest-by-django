b35d1042e1a324d6b298b69be553cbfaa7f6313b
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2022-09-23 19:43:46 -0300

    New tests were implemented

--- pyprg/core/views.py
+++ pyprg/core/views.py
@@ -9,8 +9,14 @@ DEFAULT_PAGE_SIZE = 25
 def authors(request):
     page_number = request.GET.get('page', 1)
     page_size = request.GET.get('page_size', DEFAULT_PAGE_SIZE)
+    q = request.GET.get('q')
 
-    paginator = Paginator(Author.objects.all(), per_page=page_size)
+    queryset = Author.objects.all()
+    if q:
+        # lockup do Django _icontains
+        queryset = queryset.filter(name__icontains=q)
+
+    paginator = Paginator(queryset, per_page=page_size)
     page = paginator.get_page(page_number)
 
     return JsonResponse({
